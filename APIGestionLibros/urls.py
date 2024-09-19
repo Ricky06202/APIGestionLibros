c2dcb5f4d1a304d89644c418e6a9d7af9aed7653
Azner01 <azneraparicio@hotmail.com>
2024-09-19 09:17:55 -0500

    Rest framework
    Se hizo una api sencilla usando rest framework para ir trabajando en el proceso

--- APIGestionLibros/urls.py
+++ APIGestionLibros/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("libros/", include('libros.urls')),
+    path('docs/', include_docs_urls(title='Documentación_API'))
 ]
