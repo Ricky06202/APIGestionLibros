7c93a84e297ff224585d38f81a192654ede6b5f5
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-10-03 01:36:46 -0500

    haciendo pruebas

--- libros/serializers.py
+++ libros/serializers.py
@@ -1,17 +1,40 @@
 from rest_framework import serializers
 from .models import *
 
-class LibroSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Libro
-        fields = ('__all__')
-
 class AutorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Autores
         fields = ('__all__')
-
+class AutorIdSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+        model = Autores
+        fields = ['id']
 class TemaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Temas
         fields = ('__all__')
+class TemaIdSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    class Meta:
+        model = Temas
+        fields = ['id']
+class LibroSerializer(serializers.ModelSerializer):
+    autor = AutorSerializer(many=True)
+    nombreTema = TemaSerializer(many=True)
+
+    class Meta:
+        model = Libro
+        fields = ('__all__')
+
+    def create(self, validated_data):
+        autor_data = validated_data.pop('autor')
+        tema_data = validated_data.pop('nombreTema')
+        libro = Libro.objects.create(**validated_data)
+        for a in autor_data:
+            autor, created = Autores.objects.get_or_create(**a)
+            libro.autor.add(autor)
+        for t in tema_data:
+            tema, created = Temas.objects.get_or_create(**t)
+            libro.nombreTema.add(tema)
+        return libro
